105d52d8445d6b1605337b7cd8d2f96155360b7d
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-01 00:04:19 -0400

    Implement page-not-found route

---
+++ client/src/components/routes/page-not-found/PageNotFound.tsx
@@ -0,0 +1,15 @@
+import Overview from "../../ui/Overview"
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <Overview>
+        <h1 className="text-2xl font-bold">
+          404 Error! Looks like you accessed a dead end!{" "}
+        </h1>
+      </Overview>
+    </div>
+  )
+}
+
+export default PageNotFound
