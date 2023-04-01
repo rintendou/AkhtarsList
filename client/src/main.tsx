be2c534963817d577d43d7fed4324dc81a99570a
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-01 18:43:43 -0400

    Wrap app with the AuthContextProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import { BrowserRouter as Router } from "react-router-dom"
+import AuthContextProvider from "./lib/store/AuthContext"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </Router>
   </React.StrictMode>
 )
