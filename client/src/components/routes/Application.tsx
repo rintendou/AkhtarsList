0610a13df9b574a8f765545f12072d8322d8167b
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-01 00:02:15 -0400

    Implement application route components

---
+++ client/src/components/routes/Application.tsx
@@ -0,0 +1,11 @@
+import LoginRegister from "./LoginRegister"
+
+const Application = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center">
+      <LoginRegister />
+    </div>
+  )
+}
+
+export default Application
