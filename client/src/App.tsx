fa3f5eaaf41f538ae619626b92bf94e206c0196c
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-31 23:48:13 -0400

    Set default font family

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <h1 className="text-2xl bg-red-500">Hello World!</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
