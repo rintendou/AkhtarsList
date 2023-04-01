71f389898e5463703aa63ac91bae1a336613016d
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-01 00:09:09 -0400

    Define routes of the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,40 @@
-import { useState } from "react"
-import "./App.css"
+import { Routes, Route } from "react-router-dom"
+
+// Routes
+import Application from "./components/routes/application/Application"
+import Dev from "./components/routes/dev/Dev"
+import LandingPage from "./components/routes/landing-page/LandingPage"
+import PageNotFound from "./components/routes/page-not-found/PageNotFound"
+import Login from "./components/routes/login/Login"
+import Register from "./components/routes/register/Register"
+
+// Components
+import Body from "./components/layout/body/Body"
+import Footer from "./components/layout/footer/Footer"
+import Header from "./components/layout/header/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <div className="bg-primary text-secondary">
+      <Header />
+      <Body>
+        <Routes>
+          {/*  Unprotected Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dev" element={<Dev />} />
 
-  return <div className="App">Hello World!</div>
+          {/* Protected Routes */}
+          <Route path="/app" element={<Application />} />
+
+          {/* Catch-All Route */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Body>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
