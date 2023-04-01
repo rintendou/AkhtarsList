ecb54c7e0f04dff41ab5f9b0f86d171b3e6f6e29
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-31 23:53:30 -0400

    Implement StyledInputRef component

---
+++ client/src/components/ui/StyledInputRef.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+type Props = {
+  name: string
+  type: "text" | "email" | "password"
+  placeholder: string
+}
+
+const StyledInputRef = React.forwardRef<HTMLInputElement, Props>(
+  ({ name, type = "text", placeholder, ...rest }, ref) => {
+    return (
+      <div className="relative z-0 mb-8">
+        <input
+          {...rest}
+          id={name}
+          type={type}
+          placeholder=""
+          ref={ref}
+          className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-secondary border-gray-200"
+        />
+        <label
+          htmlFor={name}
+          className="absolute duration-200 ease-in-out top-3 -z-1 origin-0 text-secondary"
+        >
+          {placeholder}
+        </label>
+      </div>
+    )
+  }
+)
+
+export default StyledInputRef
