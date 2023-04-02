87781216e3b617d355c8cd5188c4fee240ea7e2a
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 01:08:36 -0400

    Make footer dynamic based on authenticated user

--- client/src/components/layout/footer/Footer.tsx
+++ client/src/components/layout/footer/Footer.tsx
@@ -1,13 +1,25 @@
+import useAuth from "../../../lib/hooks/useAuth"
 import RouterLink from "../../ui/RouterLink"
 
 const Footer = () => {
+  const { auth } = useAuth()
+
   return (
     <footer className="px-4 py-2 flex flex-row justify-between items-center bg-secondary text-primary">
       <nav className="hidden md:flex flex-row gap-4">
-        <RouterLink to="/home" routerLinkText="Home" />
-        <RouterLink to="/login" routerLinkText="Login" />
-        <RouterLink to="/register" routerLinkText="Register" />
-        <RouterLink to="/dev" routerLinkText="Dev" />
+        {auth._id ? (
+          <>
+            <RouterLink to="/app" routerLinkText="Application" />
+            <RouterLink to="/profile" routerLinkText="Profile" />
+          </>
+        ) : (
+          <>
+            <RouterLink to="/" routerLinkText="Home" />
+            <RouterLink to="/login" routerLinkText="Login" />
+            <RouterLink to="/register" routerLinkText="Register" />
+            <RouterLink to="/dev" routerLinkText="Dev" />
+          </>
+        )}
       </nav>
       <h1 className="text-2xl font-bold">Synapse</h1>
     </footer>
