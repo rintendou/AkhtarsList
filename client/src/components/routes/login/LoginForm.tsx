7d830925974695adc6023e1c8a34987f9a595529
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-01 20:19:30 -0400

    Add address input field in registration

--- client/src/components/routes/login/LoginForm.tsx
+++ client/src/components/routes/login/LoginForm.tsx
@@ -18,12 +18,12 @@ const LoginForm = ({ didRegisterSuccessfully, successMessage }: Props) => {
 
   // I opted to use the useRef hook instead of useState to prevent
   // unnecessary re-renders of this component per each character typed
-  const emailAddressRef = useRef<HTMLInputElement>(null)
+  const usernameRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
 
   // focus on the first input on component mount
   useEffect(() => {
-    emailAddressRef.current!.focus()
+    usernameRef.current!.focus()
   }, [])
   // Keep track of error
   const [isError, setIsError] = useState(false)
@@ -36,15 +36,15 @@ const LoginForm = ({ didRegisterSuccessfully, successMessage }: Props) => {
   // the endpoint and backend_server_port number: 5178. Payload is passed
   // by attaching data to the body object.
   const loginUserHandler = () => {
-    const emailAddress = emailAddressRef.current!.value
+    const username = usernameRef.current!.value
     const password = passwordRef.current!.value
 
     const loginUser = async () => {
       const response = await fetch(`http://localhost:5178/api/auth/login`, {
         method: "POST",
         body: JSON.stringify({
-          email: emailAddress,
-          password: password,
+          username,
+          password,
         }),
         headers: { "Content-Type": "application/json" },
       })
@@ -71,10 +71,10 @@ const LoginForm = ({ didRegisterSuccessfully, successMessage }: Props) => {
       <h1 className="text-4xl font-bold text-center">Login</h1>
       <div className="flex flex-col">
         <StyledInputRef
-          name="Email Address"
-          type="email"
-          placeholder="Email Address"
-          ref={emailAddressRef}
+          name="Username"
+          type="text"
+          placeholder="Username"
+          ref={usernameRef}
         />
         <StyledInputRef
           name="Password"
