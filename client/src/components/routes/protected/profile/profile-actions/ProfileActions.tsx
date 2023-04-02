55bc9547140c0d8eaf200fe8ac00fa54e32bc962
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 16:52:06 -0400

    Navigate to appropriate payment routes

--- client/src/components/routes/protected/profile/profile-actions/ProfileActions.tsx
+++ client/src/components/routes/protected/profile/profile-actions/ProfileActions.tsx
@@ -1,15 +1,12 @@
-import { useNavigate } from "react-router-dom"
+import { redirect, useNavigate } from "react-router-dom"
 
 import StyledButton from "../../../../ui/StyledButton"
-import useProfile from "../../../../../lib/hooks/useProfile"
 
 type Props = {
   logout: () => void
 }
 
 const ProfileActions = ({ logout }: Props) => {
-  const { withdrawFunds, depositFunds } = useProfile()
-
   const navigate = useNavigate()
 
   const logoutHandler = () => {
@@ -22,12 +19,12 @@ const ProfileActions = ({ logout }: Props) => {
       <div className="flex gap-5">
         <StyledButton
           buttonText="Deposit"
-          onClick={() => depositFunds(5)}
+          onClick={() => navigate("/deposit")}
           twClasses="w-32 hover:bg-black"
         />
         <StyledButton
           buttonText="Withdraw"
-          onClick={() => withdrawFunds(5)}
+          onClick={() => navigate("/withdraw")}
           twClasses="w-32"
           intent="secondary"
         />
