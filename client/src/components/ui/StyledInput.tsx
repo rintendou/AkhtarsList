23aa02728a7f2e3a96663f60b2a399fb30b96042
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 10:41:40 -0400

    Edit StyledInput styling

--- client/src/components/ui/StyledInput.tsx
+++ client/src/components/ui/StyledInput.tsx
@@ -17,11 +17,11 @@ const StyledInput = ({
         id={name}
         type={type}
         placeholder=""
-        className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-secondary border-gray-200"
+        className="pt-3 p-2 block w-full px-0 mt-0 bg-transparent border-2 appearance-none focus:outline-none focus:ring-0 focus:border-secondary border-gray-200 pl-10"
       />
       <label
         htmlFor={name}
-        className="absolute duration-200 ease-in-out top-3 -z-1 origin-0 text-secondary"
+        className="absolute duration-200 ease-in-out top-3 left-3 -z-1 origin-0 text-secondary"
       >
         {placeholder}
       </label>
