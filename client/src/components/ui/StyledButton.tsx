4e9dacf4eb51caa7d1d9e04c37d9939897946ac3
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-01 20:35:50 -0400

    Wrap LoginForm and RegisterForm inside actual forms for better ux

--- client/src/components/ui/StyledButton.tsx
+++ client/src/components/ui/StyledButton.tsx
@@ -3,6 +3,7 @@ type Props = {
   buttonText: string
   intent?: "primary" | "secondary"
   twClasses?: string
+  type?: "button" | "submit"
 }
 
 const StyledButton = ({
@@ -10,6 +11,7 @@ const StyledButton = ({
   buttonText,
   intent = "primary",
   twClasses,
+  type = "button",
 }: Props) => {
   const isPrimaryClasses =
     intent === "primary"
@@ -20,6 +22,7 @@ const StyledButton = ({
     <button
       onClick={onClick}
       className={`px-4 py-2 rounded-lg hover:scale-105 duration-200 ease-in-out ${isPrimaryClasses} ${twClasses}`}
+      type={type}
     >
       {buttonText}
     </button>
