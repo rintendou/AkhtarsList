011478779daf5a77cad5efbfd5618fc28f6a70b4
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 19:24:03 -0400

    Abstract SideNavLinks into its own custom component

--- client/src/components/routes/categories/Category.tsx
+++ client/src/components/routes/categories/Category.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 
 import RouterLink from "../../ui/RouterLink"
+import SideNavLinks from "./SideNavLinks"
 
 const Category = () => {
   const { categoryName } = useParams()
@@ -10,38 +11,7 @@ const Category = () => {
       <div className="px-10 py-10 bg-slate-200 rounded-md shadow-md">
         <h1 className="text-3xl capitalize font-bold">{categoryName}</h1>
       </div>
-      <div className="p-5 flex flex-col gap-2 uppercase">
-        <RouterLink
-          routerLinkText="Sneakers"
-          to="/category/sneakers"
-          twClasses="text-lg"
-        />
-        <RouterLink
-          routerLinkText="Antiques"
-          to="/category/antiques"
-          twClasses="text-lg"
-        />
-        <RouterLink
-          routerLinkText="Tech"
-          to="/category/tech"
-          twClasses="text-lg"
-        />
-        <RouterLink
-          routerLinkText="Accessories"
-          to="/category/accessories"
-          twClasses="text-lg"
-        />
-        <RouterLink
-          routerLinkText="Collectibles"
-          to="/category/collectibles"
-          twClasses="text-lg"
-        />
-        <RouterLink
-          routerLinkText="Trending"
-          to="/category/trending"
-          twClasses="text-lg"
-        />
-      </div>
+      <SideNavLinks />
     </div>
   )
 }
