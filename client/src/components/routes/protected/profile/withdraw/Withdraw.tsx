c6741e4320c0f478ea3f32d4635094317846df91
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 17:57:10 -0400

    Layout struct of Withdraw route

--- client/src/components/routes/protected/profile/withdraw/Withdraw.tsx
+++ client/src/components/routes/protected/profile/withdraw/Withdraw.tsx
@@ -1,7 +1,40 @@
+import useProfile from "../../../../../lib/hooks/useProfile"
 import Card from "../../../../ui/Card"
+import StyledInputRef from "../../../../ui/StyledInputRef"
 
 const Withdraw = () => {
-  return <Card>Withdraw</Card>
+  const { balance, withdrawFunds } = useProfile()
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <Card twClasses="p-20 md:p-10 m-0 md:m-10 shadow-lg space-y-5 w-[35rem]">
+        <h1 className="text-6xl text-center font-bold">
+          Current Balance: ${`${balance}`}
+        </h1>
+        <h1 className="text-3xl text-center font-bold">Card Payment</h1>
+        <form className="flex gap-5 justify-between">
+          <StyledInputRef
+            name="Deposit Amount"
+            type="text"
+            placeholder="Deposit Amount"
+            twClasses="rounded-lg shadow-lg flex-grow"
+          />
+          <WithdrawNowButton />
+        </form>
+      </Card>
+    </div>
+  )
 }
 
 export default Withdraw
+
+const WithdrawNowButton = () => {
+  return (
+    <button
+      className={`p-4 py-2 rounded-lg duration-200 hover:bg-black ease-in-out bg-secondary text-primary font-bold text-lg shadow-xl flex-grow`}
+      type="submit"
+    >
+      Withdraw Now
+    </button>
+  )
+}
