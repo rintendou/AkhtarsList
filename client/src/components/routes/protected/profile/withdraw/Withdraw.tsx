f3352aa06d90628ed616906cc31886116e9b7578
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 18:09:58 -0400

    Add validation

--- client/src/components/routes/protected/profile/withdraw/Withdraw.tsx
+++ client/src/components/routes/protected/profile/withdraw/Withdraw.tsx
@@ -1,10 +1,14 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import useProfile from "../../../../../lib/hooks/useProfile"
+
 import Card from "../../../../ui/Card"
 import StyledInputRef from "../../../../ui/StyledInputRef"
-import { useNavigate } from "react-router-dom"
+import Error from "../../../../ui/Error"
 
 const Withdraw = () => {
+  const [error, setError] = useState("")
+
   const { balance, withdrawFunds } = useProfile()
 
   const withdrawFundsRef = useRef<HTMLInputElement>(null)
@@ -13,28 +17,43 @@ const Withdraw = () => {
 
   const withdrawFundsHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const depositAmount = Number(withdrawFundsRef.current!.value)
-    withdrawFunds(depositAmount)
+    const depositAmount = withdrawFundsRef.current!.value
+
+    if (isNaN(Number(depositAmount))) {
+      setError("Invalid Input!")
+      return
+    }
+
+    if (Number(depositAmount) > balance) {
+      setError("Insufficient Funds!")
+      return
+    }
+
+    withdrawFunds(Number(depositAmount))
     navigate("/profile", { replace: true })
   }
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
-      <Card twClasses="p-20 md:p-10 m-0 md:m-10 shadow-lg space-y-5 w-[35rem]">
+      <Card twClasses="p-20 md:p-10 m-0 md:m-10 shadow-lg space-y-5 w-[45rem]">
         <h1 className="text-6xl text-center font-bold">
           Current Balance: ${`${balance}`}
         </h1>
-        <h1 className="text-3xl text-center font-bold">Card Payment</h1>
-        <form className="flex gap-5 justify-between">
+        <h1 className="text-3xl text-center font-bold">Funds Withdraw</h1>
+        <form
+          className="flex gap-5 justify-between"
+          onSubmit={withdrawFundsHandler}
+        >
           <StyledInputRef
             ref={withdrawFundsRef}
-            name="Deposit Amount"
+            name="Withdraw Amount"
             type="text"
-            placeholder="Deposit Amount"
+            placeholder="Withdraw Amount"
             twClasses="rounded-lg shadow-lg flex-grow"
           />
           <WithdrawNowButton />
         </form>
+        {error && <Error errorMessage={error} />}
       </Card>
     </div>
   )
