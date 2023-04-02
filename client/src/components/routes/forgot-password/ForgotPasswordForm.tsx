ca47729c499420ea8b05e6f4ec4b0e8e945c9ab1
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 11:30:10 -0400

    Implement verifySecurityQAHandler

--- client/src/components/routes/forgot-password/ForgotPasswordForm.tsx
+++ client/src/components/routes/forgot-password/ForgotPasswordForm.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useRef, useState } from "react"
-import { useNavigate } from "react-router-dom"
 
 import Card from "../../ui/Card"
 import Error from "../../ui/Error"
 import StyledInputRef from "../../ui/StyledInputRef"
-import RouterLink from "../../ui/RouterLink"
+import { useNavigate } from "react-router-dom"
 
 const ForgotPasswordForm = () => {
   // I opted to use the useRef hook instead of useState to prevent
   // unnecessary re-renders of this component per each character typed
   const usernameRef = useRef<HTMLInputElement>(null)
+  const securityQuestionAnswerRef = useRef<HTMLInputElement>(null)
 
   const [isError, setIsError] = useState(false)
   const [errorMessage, setErrorMessage] = useState("")
+  const [didSubmit, setDidSubmit] = useState(false)
+  const [securityQuestion, setSecurityQuestion] = useState("")
 
-  // navigate object to redirect to another page while passing props as well
-  // when a successful registration happens, we redirect to the login page
   const navigate = useNavigate()
 
   // focus on the first input on component mount
@@ -52,26 +52,85 @@ const ForgotPasswordForm = () => {
       }
 
       setIsError(false)
+      setDidSubmit(true)
+      setSecurityQuestion(data.data)
       console.log(data)
     }
     getSecurityQuestions()
   }
 
+  const verifySecurityQAHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    // Prevent default behavior of reloading page on form submission
+    e.preventDefault()
+
+    const verifySecurityQA = async () => {
+      const username = securityQuestionAnswerRef.current!.value
+
+      const response = await fetch(
+        `http://localhost:5178/api/auth/forgot-password`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            username,
+          }),
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      const data = await response.json()
+
+      if (!data.ok) {
+        setIsError(true)
+        setErrorMessage(data.message)
+        navigate("/login", {
+          state: {
+            didRegisterSuccessfully: false,
+            errorMessage: data.message,
+          },
+        })
+        return
+      }
+
+      setIsError(false)
+      navigate("/login", {
+        state: { didRegisterSuccessfully: true, successMessage: data.message },
+      })
+      console.log(data)
+    }
+    verifySecurityQA()
+  }
+
   return (
     <Card twClasses="w-[45rem] mx-auto p-20 border-4 border-secondary space-y-16">
       <h1 className="text-4xl font-bold text-center">Reset Password</h1>
-      <form className="flex flex-col" onSubmit={getSecurityQuestionsHandler}>
-        <h1 className="text-center text-sm mb-10">
-          Please enter the username that associated with your account
-        </h1>
-        <StyledInputRef
-          name="Username"
-          type="text"
-          placeholder="Username"
-          ref={usernameRef}
-        />
-        <ForgetPasswordButton />
-      </form>
+      {!didSubmit ? (
+        <form className="flex flex-col" onSubmit={getSecurityQuestionsHandler}>
+          <h1 className="text-center text-sm mb-10">
+            Please enter the username that is associated with your account
+          </h1>
+          <StyledInputRef
+            name="Username"
+            type="text"
+            placeholder="Username"
+            ref={usernameRef}
+          />
+          <ForgetPasswordButton />
+        </form>
+      ) : (
+        <form className="flex flex-col" onSubmit={verifySecurityQAHandler}>
+          <h1 className="text-center text-sm mb-10">
+            Enter the answer to your security question
+          </h1>
+          <h1>{securityQuestion}</h1>
+          <StyledInputRef
+            name="Security Question Answer"
+            type="text"
+            placeholder="Security Question Answer"
+            ref={securityQuestionAnswerRef}
+          />
+          <SubmitButton />
+        </form>
+      )}
+
       {isError && <Error errorMessage={errorMessage} />}
     </Card>
   )
@@ -86,6 +145,17 @@ const ForgetPasswordButton = () => {
       type="submit"
     >
       Reset Password
+    </button>
+  )
+}
+
+const SubmitButton = () => {
+  return (
+    <button
+      className={`mt-4 p-4 rounded-lg duration-200 hover:bg-black ease-in-out bg-secondary text-primary font-bold text-sm`}
+      type="submit"
+    >
+      Submit Answer
     </button>
   )
 }
