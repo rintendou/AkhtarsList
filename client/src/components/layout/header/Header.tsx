554bcf30508520443956aed601162bbb30d957fe
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 00:06:46 -0400

    Add dynamic header based on login context of the user

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,14 +1,26 @@
 import RouterLink from "../../ui/RouterLink"
+import useAuth from "../../../lib/hooks/useAuth"
 
 const Header = () => {
+  const { auth } = useAuth()
+
   return (
     <header className="p-4 flex flex-row justify-between items-center bg-secondary text-primary sticky top-0">
       <h1 className="text-2xl font-bold">AkhtarsList</h1>
       <nav className="hidden md:flex flex-row gap-4">
-        <RouterLink to="/" routerLinkText="Home" />
-        <RouterLink to="/login" routerLinkText="Login" />
-        <RouterLink to="/register" routerLinkText="Register" />
-        <RouterLink to="/dev" routerLinkText="Dev" />
+        {auth._id ? (
+          <>
+            <RouterLink to="/" routerLinkText="Home" />
+            <RouterLink to="/app" routerLinkText="Application" />
+          </>
+        ) : (
+          <>
+            <RouterLink to="/" routerLinkText="Home" />
+            <RouterLink to="/login" routerLinkText="Login" />
+            <RouterLink to="/register" routerLinkText="Register" />
+            <RouterLink to="/dev" routerLinkText="Dev" />
+          </>
+        )}
       </nav>
     </header>
   )
