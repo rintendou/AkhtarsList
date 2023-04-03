0170f505600d19d5bb822f523556688a37717470
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-02 22:09:56 -0400

    Add header

--- client/src/components/routes/protected/profile/listings/Listings.tsx
+++ client/src/components/routes/protected/profile/listings/Listings.tsx
@@ -1,7 +1,9 @@
-import React from "react"
-
 const Listings = () => {
-  return <div>Listings</div>
+  return (
+    <div>
+      <h1 className="text-4xl font-bold pb-5 border-b-2 w-full">Listings</h1>
+    </div>
+  )
 }
 
 export default Listings
