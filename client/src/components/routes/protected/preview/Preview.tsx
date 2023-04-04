fcf154bbb7e84c9afe6e0a7d2fa49b1da4837073
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-04 14:09:20 -0400

    Implement Preview route

---
+++ client/src/components/routes/protected/preview/Preview.tsx
@@ -0,0 +1,11 @@
+const Preview = () => {
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <h1 className="text-3xl font-bold text-center">
+        Listing Successfully Posted!
+      </h1>
+    </div>
+  )
+}
+
+export default Preview
