197225c944ac297482e7180d250f59c560c6b84e
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-03 20:27:33 -0400

    Persist link color on current route

--- client/src/components/routes/protected/profile/profile-actions/ProfileRouterLink.tsx
+++ client/src/components/routes/protected/profile/profile-actions/ProfileRouterLink.tsx
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 type Props = {
   children: React.ReactNode
   to: string
 }
 const ProfileRouterLink = ({ children, to }: Props) => {
+  const location = useLocation()
+
   return (
     <Link
       to={to}
-      className="px-10 py-5 flex items-center hover:bg-blue-200 duration-100 ease-in-out hover:shadow-sm"
+      className={`px-10 py-5 flex items-center hover:bg-blue-200 duration-100 ease-in-out hover:shadow-sm ${
+        location.pathname === to && "bg-blue-200"
+      }`}
     >
       {children}
     </Link>
