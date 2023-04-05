3a8783200131ff642bb25ecb68ca76f6e7f8c16d
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-04 20:29:56 -0400

    Delete fetchUserListings controller fn

--- server/src/routes/listing.ts
+++ server/src/routes/listing.ts
@@ -1,20 +1,25 @@
-import express from 'express'
+import express from "express"
 
 // auth controller
-import { createListing, deleteListing, updateListing, fetchListings } from '../controllers/listingController'
+import {
+  createListing,
+  deleteListing,
+  updateListing,
+  fetchListings,
+} from "../controllers/listingController"
 
 const ListingRoute = express.Router()
 
 // Create listing
-ListingRoute.post('/post', createListing)
+ListingRoute.post("/post", createListing)
 
 // Delete listing
-ListingRoute.delete('/delete/:listingId', deleteListing)
+ListingRoute.delete("/delete/:listingId", deleteListing)
 
 // Update listing
-ListingRoute.put('/edit/:listingId', updateListing)
+ListingRoute.put("/edit/:listingId", updateListing)
 
 // Fetch all listings
-ListingRoute.get('/fetch', fetchListings)
+ListingRoute.get("/fetch", fetchListings)
 
 export default ListingRoute
