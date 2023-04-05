372e6641fd04de6ea4115acf4cac7c06d8224cad
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-05 08:28:40 -0400

    Implement Timeline child components

--- client/src/components/routes/application/timeline/AllListings.tsx
+++ client/src/components/routes/application/timeline/AllListings.tsx
@@ -1,7 +1,33 @@
-import React from "react"
+import useTimeline from "../../../../lib/hooks/useTimeline"
+
+import ListingCard from "../../../ui/ListingCard"
 
 const AllListings = () => {
-  return <div>AllListings</div>
+  const { allListings } = useTimeline()
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold pb-5 border-b-2 w-full">Listings</h1>
+      <ul className="flex gap-8 py-8 overflow-x-auto">
+        {allListings.length !== 0 ? (
+          allListings.map((listing) => (
+            <li key={listing._id}>
+              <ListingCard
+                _id={listing._id}
+                img={listing.image}
+                title={listing.title}
+                price={listing.finalPrice}
+                timeRemaining="10 days"
+                views={listing.views}
+              />
+            </li>
+          ))
+        ) : (
+          <h1 className="text-center text-2xl font-bold">No Listings Found!</h1>
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default AllListings
--- client/src/components/routes/application/timeline/TrendingListings.tsx
+++ client/src/components/routes/application/timeline/TrendingListings.tsx
@@ -1,7 +1,33 @@
-import React from "react"
+import useTimeline from "../../../../lib/hooks/useTimeline"
+
+import ListingCard from "../../../ui/ListingCard"
 
 const TrendingListings = () => {
-  return <div>TrendingListings</div>
+  const { trendingListings } = useTimeline()
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold pb-5 border-b-2 w-full">Listings</h1>
+      <ul className="flex gap-8 py-8 overflow-x-auto">
+        {trendingListings.length !== 0 ? (
+          trendingListings.map((listing) => (
+            <li key={listing._id}>
+              <ListingCard
+                _id={listing._id}
+                img={listing.image}
+                title={listing.title}
+                price={listing.finalPrice}
+                timeRemaining="10 days"
+                views={listing.views}
+              />
+            </li>
+          ))
+        ) : (
+          <h1 className="text-center text-2xl font-bold">No Listings Found!</h1>
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default TrendingListings
