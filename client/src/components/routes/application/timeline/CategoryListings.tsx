5cae315a53d86bfc5cec62c9606036d6b05a01ab
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-05 09:42:12 -0400

    Use category listings in timeline

--- client/src/components/routes/application/timeline/CategoryListings.tsx
+++ client/src/components/routes/application/timeline/CategoryListings.tsx
@@ -1,5 +1,114 @@
+import useTimeline from "../../../../lib/hooks/useTimeline"
+import ListingCard from "../../../ui/ListingCard"
+import ListingSkeletons from "../../../ui/ListingSkeletons"
+
 const CategoryListings = () => {
-  return <div></div>
+  const {
+    sneakersListings,
+    antiquesListings,
+    techListings,
+    accessoriesListings,
+    collectiblesListings,
+  } = useTimeline()
+  return (
+    <div>
+      <h1 className="text-lg font-semibold w-full">Sneakers</h1>
+      <ul className="flex gap-8 py-5 flex-wrap">
+        {sneakersListings.length !== 0 ? (
+          sneakersListings.map((listing) => (
+            <li key={listing._id}>
+              <ListingCard
+                _id={listing._id}
+                img={listing.image}
+                title={listing.title}
+                price={listing.finalPrice}
+                timeRemaining="10 days"
+                views={listing.views}
+              />
+            </li>
+          ))
+        ) : (
+          <ListingSkeletons />
+        )}
+      </ul>
+      <h1 className="text-lg font-semibold w-full">Antiques</h1>
+      <ul className="flex gap-8 py-5 flex-wrap">
+        {antiquesListings.length !== 0 ? (
+          antiquesListings.map((listing) => (
+            <li key={listing._id}>
+              <ListingCard
+                _id={listing._id}
+                img={listing.image}
+                title={listing.title}
+                price={listing.finalPrice}
+                timeRemaining="10 days"
+                views={listing.views}
+              />
+            </li>
+          ))
+        ) : (
+          <ListingSkeletons />
+        )}
+      </ul>
+      <h1 className="text-lg font-semibold w-full">Tech</h1>
+      <ul className="flex gap-8 py-5 flex-wrap">
+        {techListings.length !== 0 ? (
+          techListings.map((listing) => (
+            <li key={listing._id}>
+              <ListingCard
+                _id={listing._id}
+                img={listing.image}
+                title={listing.title}
+                price={listing.finalPrice}
+                timeRemaining="10 days"
+                views={listing.views}
+              />
+            </li>
+          ))
+        ) : (
+          <ListingSkeletons />
+        )}
+      </ul>
+      <h1 className="text-lg font-semibold w-full">Accessories</h1>
+      <ul className="flex gap-8 py-5 flex-wrap">
+        {accessoriesListings.length !== 0 ? (
+          accessoriesListings.map((listing) => (
+            <li key={listing._id}>
+              <ListingCard
+                _id={listing._id}
+                img={listing.image}
+                title={listing.title}
+                price={listing.finalPrice}
+                timeRemaining="10 days"
+                views={listing.views}
+              />
+            </li>
+          ))
+        ) : (
+          <ListingSkeletons />
+        )}
+      </ul>
+      <h1 className="text-lg font-semibold w-full">Collectibles</h1>
+      <ul className="flex gap-8 py-5 flex-wrap">
+        {collectiblesListings.length !== 0 ? (
+          collectiblesListings.map((listing) => (
+            <li key={listing._id}>
+              <ListingCard
+                _id={listing._id}
+                img={listing.image}
+                title={listing.title}
+                price={listing.finalPrice}
+                timeRemaining="10 days"
+                views={listing.views}
+              />
+            </li>
+          ))
+        ) : (
+          <ListingSkeletons />
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default CategoryListings
