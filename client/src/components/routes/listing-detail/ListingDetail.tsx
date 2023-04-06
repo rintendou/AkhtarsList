72623e82dbdde2e93b0242fccc6e9f20218f653d
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-05 21:18:44 -0400

    Set initial struct of ListingDetail

--- client/src/components/routes/listing-detail/ListingDetail.tsx
+++ client/src/components/routes/listing-detail/ListingDetail.tsx
@@ -1,11 +1,24 @@
+import { useLocation } from "react-router"
+
 const ListingDetail = () => {
+  const location = useLocation()
+
+  const { _id, img, title, price, timeRemaining, views } = location.state
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="flex-auto p-10 text-center capitalize text-2xl font-semibold max-w-none md:max-w-[50%] max-h-[50%] md:max-h-none cursor-pointer group">
-        TEST
+      <div className="flex-auto p-10 py-24 max-w-none md:max-w-[50%] max-h-[50%] md:max-h-none space-y-10 font-semibold flex flex-col items-center justify-between">
+        <div className="space-y-10">
+          <h1 className="text-3xl text-center">{title}</h1>
+          <img src={img} alt={title} />
+        </div>
+        <div className="space-y-5 text-left">
+          <p className="text-xl">Price: ${price}</p>
+          <p className="text-xl">Views: ${views}</p>
+        </div>
       </div>
-      <div className="flex-auto bg-purple-100 bg-opacity-50 p-10 max-w-none md:max-w-[50%] max-h-[50%] md:max-h-none space-y-10">
-        TEST
+      <div className="flex-auto p-10 py-20 max-w-none md:max-w-[50%] max-h-[50%] md:max-h-none space-y-10 font-semibold flex flex-col items-center justify-between">
+        <h1 className="text-3xl text-center">Biddings</h1>
       </div>
     </div>
   )
