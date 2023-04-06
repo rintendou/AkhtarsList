85ef62818fa9898898168ba35974daea6d77b637
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-06 12:39:42 -0400

    Display number of current bidders

--- client/src/components/routes/listing-detail/Bidders.tsx
+++ client/src/components/routes/listing-detail/Bidders.tsx
@@ -8,6 +8,12 @@ type BiddersProps = {
 const Bidders = ({ bidders }: BiddersProps) => {
   return (
     <ul className="space-y-5">
+      {bidders.length !== 0 && (
+        <div className="flex items-center gap-3">
+          <h1>Number of Bidders: </h1>
+          <p className="text-lg font-semibold">{bidders.length}</p>
+        </div>
+      )}
       {bidders.length !== 0 ? (
         bidders.map((bidder: UserType, index: number) => (
           <li key={index}>{bidder.username}</li>
