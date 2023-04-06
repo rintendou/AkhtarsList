1b11757c608ed00d45da6744a4fce518a6f81d47
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-06 02:00:23 -0400

    Add conditional return render when bidders is empty

--- client/src/components/routes/listing-detail/Bidders.tsx
+++ client/src/components/routes/listing-detail/Bidders.tsx
@@ -8,11 +8,17 @@ type BiddersProps = {
 
 const Bidders = ({ bidders }: BiddersProps) => {
   return (
-    <ul>
-      {bidders &&
+    <ul className="space-y-5">
+      {bidders.length !== 0 ? (
         bidders.map((bidder: UserType, index: number) => (
           <li key={index}>{bidder.username}</li>
-        ))}
+        ))
+      ) : (
+        <div className="text-center">
+          <h1 className="font-semibold text-lg">No bidders yet!</h1>
+          <p className="text-sm">Be the first one to bid!</p>
+        </div>
+      )}
     </ul>
   )
 }
