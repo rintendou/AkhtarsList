1658522bc90c492aa10c659a0ec53316f0ced082
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-07 00:31:24 -0400

    Improve EditListing component styling

--- client/src/components/routes/listing-detail/EditListing.tsx
+++ client/src/components/routes/listing-detail/EditListing.tsx
@@ -1,7 +1,7 @@
 const EditListing = () => {
   return (
     <div>
-      <h1 className="text-xs text-light text-gray-500 tracking-widest underline">
+      <h1 className="text-xs text-light text-gray-500 tracking-widest underline text-center cursor-pointer hover:text-black duration-200 ease-in-out">
         Edit Listing
       </h1>
     </div>
