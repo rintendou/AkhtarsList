e83b7071e155f22890175458720d5bd2dfcfc876
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-07 22:49:09 -0400

    Pass state and its setter fn to draganddrop instead

--- client/src/components/ui/DragAndDrop.tsx
+++ client/src/components/ui/DragAndDrop.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react"
+import React from "react"
 
 import ImageUpload from "../../../public/image-upload-undraw.svg"
 
-const DragAndDrop = () => {
-  const [imageUrl, setImageUrl] = useState<string>("")
+type Props = {
+  imageUrl: string
+  setImageUrl: React.Dispatch<React.SetStateAction<string>>
+}
 
+const DragAndDrop = ({ imageUrl, setImageUrl }: Props) => {
   const handleDrop = (event: React.DragEvent<HTMLDivElement>): void => {
     event.preventDefault()
     const file = event.dataTransfer.files[0]
@@ -40,7 +43,7 @@ const DragAndDrop = () => {
 
   return (
     <div
-      className="relative flex justify-center items-center h-[50rem] w-[50rem] max-w-full max-h-full rounded-lg cursor-pointer select-none group"
+      className="relative flex justify-center items-center h-[50rem] w-[50rem] max-w-none md:max-w-[50%] max-h-full cursor-pointer select-none group my-auto mx-5"
       onDrop={handleDrop}
       onClick={handleClick}
       onDragOver={(event) => event.preventDefault()}
@@ -86,3 +89,91 @@ const DragAndDrop = () => {
 }
 
 export default DragAndDrop
+// import React, { useState } from "react"
+
+// import ImageUpload from "../../../public/image-upload-undraw.svg"
+
+// type Props = {}
+
+// const DragAndDrop = ({ imageUrl, setImageUrl }: Props) => {
+//   const handleDrop = (event: React.DragEvent<HTMLDivElement>): void => {
+//     event.preventDefault()
+//     const file = event.dataTransfer.files[0]
+//     const reader = new FileReader()
+//     reader.onload = () => {
+//       setImageUrl(reader.result as string)
+//     }
+//     reader.readAsDataURL(file)
+//   }
+
+//   const handleRemove = (event: React.MouseEvent<HTMLButtonElement>): void => {
+//     event.stopPropagation()
+//     setImageUrl("")
+//   }
+
+//   const handleClick = (
+//     event: React.MouseEvent<HTMLDivElement, MouseEvent>
+//   ): void => {
+//     event.preventDefault()
+//     const input = document.createElement("input")
+//     input.type = "file"
+//     input.accept = "image/*"
+//     input.onchange = (e: Event) => {
+//       const file = (e.target as HTMLInputElement).files![0]
+//       const reader = new FileReader()
+//       reader.onload = () => {
+//         setImageUrl(reader.result as string)
+//       }
+//       reader.readAsDataURL(file)
+//     }
+//     input.click()
+//   }
+
+//   return (
+//     <div
+//       className="relative flex justify-center items-center h-[50rem] w-[50rem] max-w-full max-h-full rounded-lg cursor-pointer select-none group"
+//       onDrop={handleDrop}
+//       onClick={handleClick}
+//       onDragOver={(event) => event.preventDefault()}
+//     >
+//       {imageUrl ? (
+//         <>
+//           <img
+//             src={imageUrl}
+//             className="absolute inset-0 w-full h-full object-contain rounded-lg"
+//           />
+//           <button
+//             className="absolute top-2 right-2 rounded-full bg-white border-2 border-black p-1 z-10 hover:bg-black hover:border-white group duration-200 ease-in-out opacity-30"
+//             onClick={handleRemove}
+//           >
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               className="h-6 w-6 text-black group-hover:text-white"
+//               viewBox="0 0 20 20"
+//               fill="currentColor"
+//             >
+//               <path
+//                 fillRule="evenodd"
+//                 d="M10 11.414l4.95 4.95 1.414-1.414L11.414 10l4.95-4.95-1.414-1.414L10 8.586 5.05 3.636 3.636 5.05 8.586 10l-4.95 4.95 1.414 1.414L10 11.414z"
+//                 clipRule="evenodd"
+//               />
+//             </svg>
+//           </button>
+//         </>
+//       ) : (
+//         <div>
+//           <img
+//             src={ImageUpload}
+//             alt="Upload Image Here"
+//             className="h-full w-auto p-5 my-20 md:my-0 group-hover:animate-pulse"
+//           />
+//           <h1 className="text-2xl font-semibold text-center group-hover:underline duration-200 ease-in-out">
+//             Upload Listing Image Here:
+//           </h1>
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default DragAndDrop
