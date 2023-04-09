f51f3cc1033907f776763d047404bade20de7a05
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-09 18:43:29 -0400

    Implement user details form submission fn

--- client/src/components/routes/protected/settings/EditUserDetails.tsx
+++ client/src/components/routes/protected/settings/EditUserDetails.tsx
@@ -1,12 +1,38 @@
 import { useRef } from "react"
 import StyledInputRef from "../../../ui/StyledInputRef"
 import PasswordInputRef from "../../../ui/PasswordInputRef"
+import { settings } from "../../../../settings"
 
 const EditUserDetails = () => {
   const addressRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
 
-  const editUserDetailsHandler = () => {}
+  const editUserDetailsHandler = () => {
+    const address = addressRef.current!.value
+    const password = passwordRef.current!.value
+
+    const payload = {
+      address,
+      password,
+    }
+
+    const editUserDetails = async () => {
+      const response = await fetch(
+        `http://localhost:${settings.BACKEND_SERVER_PORT}/api/auth/change/user-details`,
+        {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      const json = await response.json()
+
+      if (!json.ok) {
+        return
+      }
+    }
+    editUserDetails()
+  }
 
   return (
     <div className="space-y-5">
