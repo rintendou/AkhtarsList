456a5b188a32bb7d80196a7fc68a70dc9bc50d05
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-09 01:50:41 -0400

    Implement admin actions component

--- client/src/components/routes/protected/profile/profile-actions/ProfileActions.tsx
+++ client/src/components/routes/protected/profile/profile-actions/ProfileActions.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import ProfileButton from "./ProfileButton"
 import ProfileRouterLink from "./ProfileRouterLink"
 import useAuth from "../../../../../lib/hooks/useAuth"
+import AdminActions from "./admin-actions/AdminActions"
 
 type Props = {
   logout: () => void
@@ -120,52 +121,7 @@ const ProfileActions = ({ logout }: Props) => {
         </div>
       </ProfileRouterLink>
 
-      {auth.isAdmin && (
-        <>
-          <ProfileRouterLink to="/disputed-listings">
-            <svg
-              className="mr-4"
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 24 24"
-              height="2.4em"
-              width="2.4em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g>
-                <path fill="none" d="M0 0h24v24H0z"></path>
-                <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-1-5h2v2h-2v-2zm0-8h2v6h-2V7z"></path>
-              </g>
-            </svg>
-            <div className="text-xs">
-              <h1 className="text-lg font-semibold">Disputed Listings</h1>
-              <p className="text-gray-500">Manage undelivered items</p>
-            </div>
-          </ProfileRouterLink>
-          <ProfileRouterLink to="/disputed-listings">
-            <svg
-              className="mr-4"
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 24 24"
-              height="2.4em"
-              width="2.4em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g>
-                <path fill="none" d="M0 0h24v24H0z"></path>
-                <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-1-5h2v2h-2v-2zm0-8h2v6h-2V7z"></path>
-              </g>
-            </svg>
-            <div className="text-xs">
-              <h1 className="text-lg font-semibold">Disputed Listings</h1>
-              <p className="text-gray-500">Manage undelivered items</p>
-            </div>
-          </ProfileRouterLink>
-        </>
-      )}
+      {auth.isAdmin && <AdminActions />}
 
       <ProfileButton onClick={logoutHandler}>
         <svg
--- client/src/components/routes/protected/profile/profile-actions/admin-actions/AdminActions.tsx
+++ client/src/components/routes/protected/profile/profile-actions/admin-actions/AdminActions.tsx
@@ -1,5 +1,56 @@
+import AdminRouterLink from "./AdminRouterLink"
+
 const AdminActions = () => {
-  return <div>AdminActions</div>
+  return (
+    <>
+      <AdminRouterLink to="/admin/manage-listings">
+        <svg
+          className="mr-4"
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 24 24"
+          height="2.4em"
+          width="2.4em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="none"
+            stroke="#000"
+            strokeWidth="2"
+            d="M16,16 C16,14.8954305 12.8659932,14 9,14 C5.13400675,14 2,14.8954305 2,16 C2,17.1045695 5.13400675,18 9,18 C12.8659932,18 16,17.1045695 16,16 Z M2,16 L2,20.9367547 C2,22.0762536 5.13400675,23 9,23 C12.8659932,23 16,22.0762537 16,20.9367548 L16,16 M9,5 C4.581722,5 1,5.8954305 1,7 C1,8.1045695 4.581722,9 9,9 M1,7 L1,12.0000002 C1,13.0128881 4.581722,14 9,14 M23,4 C23,2.8954305 19.9004329,2 16.0769231,2 C12.2534133,2 9.15384615,2.8954305 9.15384615,4 C9.15384615,5.1045695 12.2534133,6 16.0769231,6 C19.9004329,6 23,5.1045695 23,4 Z M16,16 C19.8235098,16 23.0000002,15.0128879 23.0000002,14 L23,4 M9.15384615,3.99999999 L9.15384615,14.1660042 M8.99999999,9.00000001 C8.99999999,10.0128879 12.2534135,11 16.0769233,11 C19.9004331,11 23.0000004,10.0128879 23.0000004,9.00000001"
+          ></path>
+        </svg>
+        <div className="text-xs">
+          <h1 className="text-lg font-semibold">Manage listings</h1>
+          <p className="text-gray-500">Successful transactions</p>
+        </div>
+      </AdminRouterLink>
+      <AdminRouterLink to="/admin/disputed-listings">
+        <svg
+          className="mr-4"
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 24 24"
+          height="2.4em"
+          width="2.4em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="none"
+            stroke="#000"
+            strokeWidth="2"
+            d="M16,12 C18.3736719,13.1826446 20,15.6506255 20,19 L20,23 L4,23 L4,19 C4,15.6457258 5.6310898,13.1754259 8,12 M12,13 C15.3137085,13 18,10.3137085 18,7 C18,3.6862915 15.3137085,1 12,1 C8.6862915,1 6,3.6862915 6,7 C6,10.3137085 8.6862915,13 12,13 Z M18,7 C16.5,7 15,7.3599999 13,5 C11,7.3599999 8.5,8 6,7 M7,13 L12.0249378,18.2571942 L17,13 M12,18 L12,23"
+          ></path>
+        </svg>
+        <div className="text-xs">
+          <h1 className="text-lg font-semibold">Regulate Disputes</h1>
+          <p className="text-gray-500">Undelivered listings</p>
+        </div>
+      </AdminRouterLink>
+    </>
+  )
 }
 
 export default AdminActions
