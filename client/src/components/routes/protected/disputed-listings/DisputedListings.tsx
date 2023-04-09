45ec7438fd81ed7a90d55b1a62c52eb475292097
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-09 01:20:17 -0400

    Set new routes to implement

---
+++ client/src/components/routes/protected/disputed-listings/DisputedListings.tsx
@@ -0,0 +1,9 @@
+const DisputedListings = () => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      DisputedListings
+    </div>
+  )
+}
+
+export default DisputedListings
---
+++ client/src/components/routes/protected/won-listings/WonListings.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const WonListings = () => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      WonListings
+    </div>
+  )
+}
+
+export default WonListings
