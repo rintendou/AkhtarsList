232df833142ef9179de2c5055cd645bce233841f
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-09 18:48:02 -0400

    Implement change password form submit fn

--- client/src/components/routes/protected/settings/ChangePassword.tsx
+++ client/src/components/routes/protected/settings/ChangePassword.tsx
@@ -1,20 +1,45 @@
 import { useRef } from "react"
 import PasswordInputRef from "../../../ui/PasswordInputRef"
+import { settings } from "../../../../settings"
 
 const ChangePassword = () => {
   const oldPasswordRef = useRef<HTMLInputElement>(null)
   const newPasswordRef = useRef<HTMLInputElement>(null)
   const newConfirmPasswordRef = useRef<HTMLInputElement>(null)
 
-  const changeSecurityQuestionsHandler = () => {}
+  const changePasswordHandler = () => {
+    const oldPassword = oldPasswordRef.current!.value
+    const newPassword = newPasswordRef.current!.value
+    const newConfirmPassword = newConfirmPasswordRef.current!.value
+
+    const payload = {
+      oldPassword,
+      newPassword,
+      newConfirmPassword,
+    }
+
+    const changePassword = async () => {
+      const response = await fetch(
+        `http://localhost:${settings.BACKEND_SERVER_PORT}/api/auth/change/password`,
+        {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      const json = await response.json()
+
+      if (!json.ok) {
+        return
+      }
+    }
+    changePassword()
+  }
 
   return (
     <div className="space-y-5">
       <h1 className="text-xl font-semibold">Change Password</h1>
-      <form
-        className="flex flex-col gap-5"
-        onSubmit={changeSecurityQuestionsHandler}
-      >
+      <form className="flex flex-col gap-5" onSubmit={changePasswordHandler}>
         <PasswordInputRef name="Old Password" ref={oldPasswordRef} />
         <PasswordInputRef name="New Password" ref={newPasswordRef} />
         <PasswordInputRef name="Confirm Password" ref={newConfirmPasswordRef} />
