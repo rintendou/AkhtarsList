865dae5ccb5990e206da34a8f27848faa4c8f53a
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-09 01:44:56 -0400

    Implement admin router link

--- client/src/components/routes/protected/profile/profile-actions/admin-actions/AdminRouterLink.tsx
+++ client/src/components/routes/protected/profile/profile-actions/admin-actions/AdminRouterLink.tsx
@@ -1,5 +1,22 @@
-const AdminRouterLink = () => {
-  return <div>AdminRouterLink</div>
+import { Link, useLocation } from "react-router-dom"
+
+type Props = {
+  children: React.ReactNode
+  to: string
+}
+const AdminRouterLink = ({ children, to }: Props) => {
+  const location = useLocation()
+
+  return (
+    <Link
+      to={to}
+      className={`px-10 py-5 flex items-center bg-secondary text-primary hover:bg-tertiary duration-100 ease-in-out hover:shadow-sm ${
+        location.pathname === to && "bg-tertiary"
+      }`}
+    >
+      {children}
+    </Link>
+  )
 }
 
 export default AdminRouterLink
