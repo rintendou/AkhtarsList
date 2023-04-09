9463f59f1ec892e04569ec106ac0d51c11e0cd05
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-09 02:44:52 -0400

    Scroll to top on component mount

--- client/src/components/routes/protected/admin/ManageDisputes.tsx
+++ client/src/components/routes/protected/admin/ManageDisputes.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+
 const ManageDisputes = () => {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }, [])
   return <div>ManageDisputes</div>
 }
 
--- client/src/components/routes/protected/admin/ManageListings.tsx
+++ client/src/components/routes/protected/admin/ManageListings.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+
 const ManageListings = () => {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }, [])
   return <div>ManageListings</div>
 }
 
