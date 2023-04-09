5dd901f6f81695da1539edd46587fe24ab187a5d
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-09 01:22:42 -0400

    Implement won and disputed listings route

--- client/src/components/routes/protected/disputed-listings/DisputedListings.tsx
+++ client/src/components/routes/protected/disputed-listings/DisputedListings.tsx
@@ -1,7 +1,36 @@
+import useProfile from "../../../../lib/hooks/useProfile"
+import ListingCard from "../../../ui/ListingCard"
+
 const DisputedListings = () => {
+  const { disputedListings } = useProfile()
+
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      DisputedListings
+      <ul className="flex gap-8 py-8 overflow-x-auto">
+        {disputedListings.length !== 0 &&
+          disputedListings.map((disputedListing) => (
+            <li key={disputedListing._id}>
+              <ListingCard
+                _id={disputedListing._id}
+                image={disputedListing.image}
+                title={disputedListing.title}
+                finalPrice={disputedListing.finalPrice}
+                expireAt={disputedListing.expireAt}
+                views={disputedListing.views}
+                bidders={disputedListing.bidders}
+                lister={disputedListing.lister}
+                desc={disputedListing.desc}
+                startPrice={disputedListing.startPrice}
+                category={disputedListing.category}
+                weight={disputedListing.weight}
+                dimensions={disputedListing.dimensions}
+                height={disputedListing.height}
+                width={disputedListing.width}
+                length={disputedListing.length}
+              />
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
--- client/src/components/routes/protected/won-listings/WonListings.tsx
+++ client/src/components/routes/protected/won-listings/WonListings.tsx
@@ -1,9 +1,36 @@
-import React from "react"
+import useProfile from "../../../../lib/hooks/useProfile"
+import ListingCard from "../../../ui/ListingCard"
 
 const WonListings = () => {
+  const { wonListings } = useProfile()
+
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      WonListings
+      <ul className="flex gap-8 py-8 overflow-x-auto">
+        {wonListings.length !== 0 &&
+          wonListings.map((wonListing) => (
+            <li key={wonListing._id}>
+              <ListingCard
+                _id={wonListing._id}
+                image={wonListing.image}
+                title={wonListing.title}
+                finalPrice={wonListing.finalPrice}
+                expireAt={wonListing.expireAt}
+                views={wonListing.views}
+                bidders={wonListing.bidders}
+                lister={wonListing.lister}
+                desc={wonListing.desc}
+                startPrice={wonListing.startPrice}
+                category={wonListing.category}
+                weight={wonListing.weight}
+                dimensions={wonListing.dimensions}
+                height={wonListing.height}
+                width={wonListing.width}
+                length={wonListing.length}
+              />
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
