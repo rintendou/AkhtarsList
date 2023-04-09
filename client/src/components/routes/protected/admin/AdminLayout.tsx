ba28015a7d9b0cd82550c4af358fcd3dbb54369c
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-09 01:55:25 -0400

    Implement admin layout as a nav guard by requiring isAdmin

---
+++ client/src/components/routes/protected/admin/AdminLayout.tsx
@@ -0,0 +1,16 @@
+import { useLocation, Outlet, Navigate } from "react-router-dom"
+import useAuth from "../../../../lib/hooks/useAuth"
+
+const RequireAuth = () => {
+  const { auth } = useAuth()
+
+  const location = useLocation()
+
+  return auth?.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  )
+}
+
+export default RequireAuth
