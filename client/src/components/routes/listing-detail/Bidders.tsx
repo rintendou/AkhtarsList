f030f84acd40ee92a5a8472301e7e92e43c8db39
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-11 15:45:18 -0400

    Refactor bidders section of listing detail

--- client/src/components/routes/listing-detail/Bidders.tsx
+++ client/src/components/routes/listing-detail/Bidders.tsx
@@ -18,37 +18,43 @@ const Bidders = () => {
   )
 
   return (
-    <ul className="space-y-5 w-full overflow-y-auto h-96 border-2 p-4 border-secondary rounded-md">
-      {bidders.length !== 0 && (
-        <div className="flex items-center gap-3">
+    <div className="w-full">
+      <h1 className="text-center text-2xl font-semibold">Bidders</h1>
+      <div className="space-y-5 w-full overflow-y-auto h-96 border-2 p-4 border-secondary rounded-md">
+        <div className="flex items-center space-x-4">
           <h1>Number of Bidders: </h1>
           <p className="text-lg font-semibold">{bidders.length}</p>
         </div>
-      )}
-      {bidders.length !== 0 ? (
-        bidders.map((bidder: string, index: number) => (
-          <li
-            key={index}
-            className={`${
-              index === 0 ? "text-black font-bold" : "text-gray-500"
-            } flex items-center gap-3`}
-          >
-            {index + 1}.) {bidder}
-            {index === 0 && CrownSVG}
-          </li>
-        ))
-      ) : isLister ? (
-        <div className="text-center">
-          <h1 className="font-semibold text-lg">No bidders yet!</h1>
-          <p className="text-sm">Waiting for other users to bid!</p>
-        </div>
-      ) : (
-        <div className="text-center">
-          <h1 className="font-semibold text-lg">No bidders yet!</h1>
-          <p className="text-sm">Be the first one to bid!</p>
-        </div>
-      )}
-    </ul>
+
+        <ul>
+          {bidders.length !== 0 && (
+            <div className="flex items-center gap-3"></div>
+          )}
+          {bidders.length !== 0 ? (
+            bidders.map((bidder: string, index: number) => (
+              <li
+                key={index}
+                className={`${
+                  index === 0 ? "text-black font-bold" : "text-gray-500"
+                } flex items-center gap-3`}
+              >
+                {index + 1}.) {bidder}
+                {index === 0 && CrownSVG}
+              </li>
+            ))
+          ) : (
+            <div className="text-center">
+              <h1 className="font-semibold text-lg">No bidders yet!</h1>
+              {isLister ? (
+                <p className="text-sm">Waiting for other users to bid!</p>
+              ) : (
+                <p className="text-sm">Be the first one to bid!</p>
+              )}
+            </div>
+          )}
+        </ul>
+      </div>
+    </div>
   )
 }
 
