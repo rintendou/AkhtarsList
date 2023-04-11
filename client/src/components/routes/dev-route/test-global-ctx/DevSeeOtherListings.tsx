216b915b53ffe23db19dbe2297c4b1fffd1e05c4
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-11 13:15:53 -0400

    Finalize dev listing detail by not passing props but to use context

---
+++ client/src/components/routes/dev-route/test-global-ctx/DevSeeOtherListings.tsx
@@ -0,0 +1,131 @@
+// Components
+
+// Types
+
+import { useNavigate } from "react-router-dom"
+import useTimeline from "../../../../lib/hooks/useTimeline"
+import ListingType from "../../../../lib/types/ListingType"
+import SeeOthersButton from "../../listing-detail/SeeOthersButton"
+import ListingCard from "../../../ui/ListingCard"
+import ListMore from "../../protected/profile/listings/ListMore"
+import useListingDetail from "./useListingDetail"
+
+const DevSeeOtherListings = () => {
+  const {
+    allListings,
+    trendingListings,
+    sneakersListings,
+    antiquesListings,
+    techListings,
+    accessoriesListings,
+    collectiblesListings,
+    fetchListingsByCategory,
+  } = useTimeline()
+
+  const { listing } = useListingDetail()
+  const { _id: idToFilter, category } = listing
+
+  let categorizedListings: ListingType[] = []
+
+  switch (category.toLowerCase()) {
+    case "sneakers":
+      categorizedListings = sneakersListings
+      break
+    case "antiques":
+      categorizedListings = antiquesListings
+      break
+    case "tech":
+      categorizedListings = techListings
+      break
+    case "accessories":
+      categorizedListings = accessoriesListings
+      break
+    case "collectibles":
+      categorizedListings = collectiblesListings
+      break
+    case "trending":
+      categorizedListings = trendingListings
+      break
+    case "assorted":
+      categorizedListings = allListings
+      break
+  }
+
+  const filteredAndCategorizedListings = categorizedListings.filter(
+    (l) => l._id !== idToFilter
+  )
+
+  const navigate = useNavigate()
+
+  const onListingClick = (listing: ListingType) => {
+    fetchListingsByCategory(category)
+    navigate(`/listings/${listing._id}`, {
+      state: {
+        id: listing._id,
+        image: listing.image,
+        bidders: listing.bidders,
+        lister: listing.lister,
+        desc: listing.desc,
+        title: listing.title,
+        startPrice: listing.startPrice,
+        finalPrice: listing.finalPrice,
+        expireAt: listing.expireAt,
+        views: listing.views,
+        category: listing.category,
+        dimensions: listing.dimensions,
+        weight: listing.weight,
+        height: listing.height,
+        width: listing.width,
+        length: listing.length,
+      },
+    })
+  }
+
+  return (
+    <div className="mx-auto container my-10">
+      <div className="flex justify-between">
+        <h1 className="text-lg font-semibold w-full">
+          See Other Listings in {category}
+        </h1>
+        <SeeOthersButton to={`/category/${category.toLowerCase()}`} />
+      </div>
+      <ul className="flex gap-8 py-5 overflow-x-scroll">
+        {filteredAndCategorizedListings.length !== 0 ? (
+          filteredAndCategorizedListings.map((listing) => (
+            <li key={listing._id}>
+              <div onClick={() => onListingClick(listing)}>
+                <ListingCard
+                  _id={listing._id}
+                  image={listing.image}
+                  title={listing.title}
+                  finalPrice={listing.finalPrice}
+                  expireAt={listing.expireAt}
+                  views={listing.views}
+                  bidders={listing.bidders}
+                  lister={listing.lister}
+                  desc={listing.desc}
+                  startPrice={listing.startPrice}
+                  category={listing.category}
+                  weight={listing.weight}
+                  dimensions={listing.dimensions}
+                  height={listing.height}
+                  width={listing.width}
+                  length={listing.length}
+                />
+              </div>
+            </li>
+          ))
+        ) : (
+          <div>
+            <div>
+              <h1>No Other Listings Found in {category}</h1>
+            </div>
+            <ListMore />
+          </div>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default DevSeeOtherListings
