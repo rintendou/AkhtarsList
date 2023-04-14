adcd23c9f5dc09934b1f47643761c20fc58ddee8
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-13 22:21:06 -0400

    Duplicate listing detail to be implement with react-query

---
+++ client/src/components/routes/listing-detail-query/ListingDetail.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react"
+import useListingDetail from "../../../lib/hooks/useListingDetail"
+
+// Assets
+import SeeOtherListings from "./SeeOtherListings"
+import ListingOverview from "./ListingOverview"
+import ExpiredBiddingSection from "./ExpiredBiddingSection"
+import ActiveBiddingSection from "./ActiveBiddingSection"
+import ListingDetailSkeleton from "./ListingDetailSkeleton"
+
+const ListingDetail = () => {
+  const { isExpired, isLoading } = useListingDetail()
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }, [])
+
+  const biddingSection = isExpired ? (
+    <ExpiredBiddingSection />
+  ) : (
+    <ActiveBiddingSection />
+  )
+
+  return (
+    <div>
+      {!isLoading ? (
+        <>
+          <div className="flex flex-col md:flex-row min-h-screen border-b-2 border-b-tertiary">
+            <ListingOverview />
+            {biddingSection}
+          </div>
+          <SeeOtherListings />
+        </>
+      ) : (
+        <ListingDetailSkeleton />
+      )}
+    </div>
+  )
+}
+
+export default ListingDetail
