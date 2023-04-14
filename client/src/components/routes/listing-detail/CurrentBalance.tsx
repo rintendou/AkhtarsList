fce7f4bed67744ef7233041fcf837a80d52cf69c
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-14 01:38:29 -0400

    Implement current balance component

--- client/src/components/routes/listing-detail/CurrentBalance.tsx
+++ client/src/components/routes/listing-detail/CurrentBalance.tsx
@@ -1,7 +1,14 @@
-import React from "react"
+import useProfile from "../../../lib/hooks/useProfile"
 
 const CurrentBalance = () => {
-  return <div>CurrentBalance</div>
+  const { balance } = useProfile()
+
+  return (
+    <div className="text-center flex gap-5">
+      Your current balance is: $
+      <h1 className="text-lg font-semibold">{balance}</h1>
+    </div>
+  )
 }
 
 export default CurrentBalance
