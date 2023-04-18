02b481b55cfd06a014b928c07ce74a5fd37399f9
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-18 19:09:32 -0400

    Change scope of the global context providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,24 +4,13 @@ import "./index.css"
 import { BrowserRouter as Router } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-// Global State
-import AuthContextProvider from "./lib/store/AuthContext"
-import ProfileContextProvider from "./lib/store/ProfileContext"
-import TimelineContextProvider from "./lib/store/TimelineContext"
-
 // Query Library
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Router>
     <QueryClientProvider client={queryClient}>
-      <TimelineContextProvider>
-        <AuthContextProvider>
-          <ProfileContextProvider>
-            <App />
-          </ProfileContextProvider>
-        </AuthContextProvider>
-      </TimelineContextProvider>
+      <App />
     </QueryClientProvider>
   </Router>
 )
