f7c18e4da9b2adb4ab8f666448ec2d0b159b298c
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-18 18:54:23 -0400

    Show search bar even if no user auth-ed

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -3,7 +3,7 @@ import useAuth from "../../../lib/hooks/context-hooks/useAuthContext"
 import SearchBar from "./SearchBar"
 
 const Header = () => {
-  const { auth } = useAuth()
+  const { isLoggedIn } = useAuth()
 
   return (
     <header className="p-8 flex flex-row justify-between items-center bg-secondary text-primary sticky top-0 gap-8 z-50">
@@ -12,9 +12,9 @@ const Header = () => {
         routerLinkText="AkhtarsList"
         to="/"
       />
-      {auth._id && <SearchBar />}
+      <SearchBar />
       <nav className="hidden md:flex flex-row gap-4">
-        {auth._id ? (
+        {isLoggedIn ? (
           <LoggedInNavbar />
         ) : (
           <>
