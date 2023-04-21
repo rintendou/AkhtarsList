b8ffc3f22c711d190d27a31c937e69fd71fc3c19
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-21 16:01:35 -0400

    Layout admin dashboard

--- client/src/components/routes/protected/profile/profile-actions/admin-actions/AdminActions.tsx
+++ client/src/components/routes/protected/profile/profile-actions/admin-actions/AdminActions.tsx
@@ -3,15 +3,15 @@ import AdminRouterLink from "./AdminRouterLink"
 const AdminActions = () => {
   return (
     <div>
-      <AdminRouterLink to="/admin/manage-listings">
+      <AdminRouterLink to="/admin">
         <svg
           className="mr-4"
           stroke="currentColor"
           fill="currentColor"
           strokeWidth="0"
           viewBox="0 0 24 24"
-          height="2.4em"
-          width="2.4em"
+          height="2.2em"
+          width="2.2em"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
@@ -22,31 +22,8 @@ const AdminActions = () => {
           ></path>
         </svg>
         <div className="text-xs">
-          <h1 className="text-lg font-semibold">Manage listings</h1>
-          <p className="text-primary">Successful transactions</p>
-        </div>
-      </AdminRouterLink>
-      <AdminRouterLink to="/admin/manage-disputes">
-        <svg
-          className="mr-4"
-          stroke="currentColor"
-          fill="currentColor"
-          strokeWidth="0"
-          viewBox="0 0 24 24"
-          height="2.4em"
-          width="2.4em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill="none"
-            stroke="#00000"
-            strokeWidth="2"
-            d="M16,12 C18.3736719,13.1826446 20,15.6506255 20,19 L20,23 L4,23 L4,19 C4,15.6457258 5.6310898,13.1754259 8,12 M12,13 C15.3137085,13 18,10.3137085 18,7 C18,3.6862915 15.3137085,1 12,1 C8.6862915,1 6,3.6862915 6,7 C6,10.3137085 8.6862915,13 12,13 Z M18,7 C16.5,7 15,7.3599999 13,5 C11,7.3599999 8.5,8 6,7 M7,13 L12.0249378,18.2571942 L17,13 M12,18 L12,23"
-          ></path>
-        </svg>
-        <div className="text-xs">
-          <h1 className="text-lg font-semibold">Regulate Disputes</h1>
-          <p className="text-primary">Undelivered listings</p>
+          <h1 className="text-lg font-semibold">Admin Dashboard</h1>
+          <p className="text-primary">Manage and Regulate Listings</p>
         </div>
       </AdminRouterLink>
     </div>
