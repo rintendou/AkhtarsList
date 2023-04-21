bee3a266984a6c1501ddc300401c6adb3461a6e7
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-21 02:18:00 -0400

    Add button to contact lister

--- client/src/components/routes/listing-detail/ExpiredBiddingSection.tsx
+++ client/src/components/routes/listing-detail/ExpiredBiddingSection.tsx
@@ -1,6 +1,7 @@
 // Components
 import useAuthContext from "../../../lib/hooks/context-hooks/useAuthContext"
 import useListingDetailContextQuery from "../../../lib/hooks/context-hooks/useListingDetailContext"
+import StyledButton from "../../ui/StyledButton"
 import Bidders from "./Bidders"
 
 import Transactions from "./Transactions"
@@ -13,19 +14,21 @@ const ExpiredBiddingSection = () => {
 
   const isLister = lister === auth._id
 
+  const isWinner = listing.bestBidder === auth._id
+
   return (
     <div
-      className={`flex-auto p-10 py-24 max-w-none md:max-w-[50%] max-h-[50%] md:max-h-none space-y-10 flex flex-col items-center bg-purple-100 opacity-40 select-none`}
+      className={`flex-auto p-10 py-24 max-w-none md:max-w-[50%] max-h-[50%] md:max-h-none space-y-10 flex flex-col items-center bg-purple-100 bg-opacity-40 select-none`}
     >
-      <h1 className="text-5xl text-center font-semibold opacity-100">
+      <h1 className="text-5xl text-center font-semibold  opacity-40">
         Listing Expired
       </h1>
 
-      <h1 className="text-5xl text-center font-semibold backdrop-opacity-30">
+      <h1 className="text-5xl text-center font-semibold backdrop-opacity-30 opacity-40">
         Biddings
       </h1>
 
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between w-full opacity-40">
         <div className="flex items-center gap-3">
           <h1>Final Price:</h1>
           <p className="text-lg font-semibold"> ${finalPrice}</p>
@@ -36,22 +39,35 @@ const ExpiredBiddingSection = () => {
       </div>
 
       {isLister ? (
-        <div>
+        <div className="opacity-40">
           <h1 className="text-3xl font-semibold">You own this listing</h1>
           <h1 className="text-xs font-light text-gray-500 tracking-widest line-through text-center cursor-not-allowed duration-200 ease-in-out hover:font-semibold uppercase">
             Cannot Edit Expired Listing
           </h1>
         </div>
       ) : (
-        <h1 className="text-3xl font-semibold text-center">
+        <h1 className="text-3xl font-semibold text-center opacity-40">
           Bidding has finished
         </h1>
       )}
 
-      <div className="w-full flex flex-col lg:flex-row gap-5">
+      <div className="w-full flex flex-col lg:flex-row gap-5 opacity-40">
         <Bidders />
         <Transactions />
       </div>
+
+      {isWinner && (
+        <div className="w-full">
+          <h1 className="text-center font-semibold text-lg">
+            You won this listing!
+          </h1>
+          <StyledButton
+            buttonText="Contact Lister"
+            onClick={() => {}}
+            twClasses="text-2xl py-4 w-full hover:bg-black"
+          />
+        </div>
+      )}
     </div>
   )
 }
