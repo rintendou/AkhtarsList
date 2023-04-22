18a5c7025e54391dd59ed71388059326cb4210c7
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-22 15:15:56 -0400

    Impleemnt disputes report

--- client/src/components/routes/protected/admin/disputes-to-manage/DisputesToManage.tsx
+++ client/src/components/routes/protected/admin/disputes-to-manage/DisputesToManage.tsx
@@ -1,5 +1,6 @@
 import useTimelineContext from "../../../../../lib/hooks/context-hooks/useTimelineContext"
 import ListingCard from "../../../../ui/ListingCard"
+import DisputesReport from "./DisputesReport"
 
 const DisputesToManage = () => {
   const { expiredListings } = useTimelineContext()
@@ -11,6 +12,7 @@ const DisputesToManage = () => {
   return (
     <div className="flex flex-col w-full p-5 gap-10">
       <h1 className="text-4xl font-bold pb-5 border-b-2 w-full">Disputes</h1>
+      <DisputesReport />
       <ul className="flex gap-8 py-8 overflow-x-auto px-8">
         {disputes.length !== 0 ? (
           disputes.map((dispute) => (
