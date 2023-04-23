25ec0389e8616e3a18c5fc1098c773430b228bd8
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-22 21:26:28 -0400

    Add back to profile button on admin actions

--- client/src/components/routes/protected/admin/admin-actions/AdminActions.tsx
+++ client/src/components/routes/protected/admin/admin-actions/AdminActions.tsx
@@ -1,3 +1,4 @@
+import ProfileRouterLink from "../../profile/profile-actions/ProfileRouterLink"
 import AdminRouterLink from "./AdminRouterLink"
 
 const AdminActions = () => {
@@ -98,6 +99,27 @@ const AdminActions = () => {
           <p>Manage Reported Listings</p>
         </div>
       </AdminRouterLink>
+      <ProfileRouterLink to="/profile">
+        <svg
+          className="mr-4"
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 24 24"
+          height="2.4em"
+          width="2.4em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g>
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M2 3.993A1 1 0 0 1 2.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 0 1-.992.993H2.992A.993.993 0 0 1 2 20.007V3.993zM4 5v14h16V5H4zm2 2h6v6H6V7zm2 2v2h2V9H8zm-2 6h12v2H6v-2zm8-8h4v2h-4V7zm0 4h4v2h-4v-2z"></path>
+          </g>
+        </svg>
+        <div className="text-xs">
+          <h1 className="text-lg font-semibold">Profile</h1>
+          <p className="text-gray-500 dark:text-white">See your details</p>
+        </div>
+      </ProfileRouterLink>
     </div>
   )
 }
