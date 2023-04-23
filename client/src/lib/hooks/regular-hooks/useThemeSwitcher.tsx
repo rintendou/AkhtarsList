17af1e4de82f8fbe3b3c624e7240b1a82579a4b3
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-22 20:13:18 -0400

    Implement custom useThemeSwitcher hook

---
+++ client/src/lib/hooks/regular-hooks/useThemeSwitcher.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react"
+
+const useThemeSwitcher = () => {
+  const [mode, setMode] = useState(() => localStorage.getItem("mode"))
+
+  useEffect(() => {
+    window.addEventListener("storage", setPreferedTheme)
+    return () => {
+      window.removeEventListener("storage", setPreferedTheme)
+    }
+  }, [])
+
+  const setPreferedTheme = () => {
+    const _mode = localStorage.getItem("mode")
+    if (_mode) {
+      setMode(_mode)
+    } else {
+      setMode("light")
+    }
+  }
+
+  useEffect(() => {
+    if (mode === "dark") {
+      document.body.classList.add("dark-mode")
+      localStorage.setItem("mode", "dark")
+    } else {
+      document.body.classList.remove("dark-mode")
+      localStorage.setItem("mode", "light")
+    }
+  }, [mode])
+
+  return (
+    <a
+      className="cursor-pointer"
+      onClick={() => setMode((mode) => (mode === "dark" ? "light" : "dark"))}
+    >
+      <div> {mode === "dark" ? "Light" : "Dark"} Mode</div>
+    </a>
+  )
+}
+
+export default useThemeSwitcher
