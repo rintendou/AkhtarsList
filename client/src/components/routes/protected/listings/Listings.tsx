1f8192531f5ccf0d3f0d02226357aa2985e1e212
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-24 01:02:33 -0400

    Scroll to top on component mount

--- client/src/components/routes/protected/listings/Listings.tsx
+++ client/src/components/routes/protected/listings/Listings.tsx
@@ -4,10 +4,12 @@ import ListingsToFulfill from "./ListingsToFulfill"
 import SoldListings from "./SoldListings"
 import AllListings from "./AllListings"
 import ListingsOverview from "./ListingsOverview"
+import ScrollToTop from "../../../../lib/util/components/ScrollToTop"
 
 const Listings = () => {
   return (
     <div className="flex flex-col w-full p-5 gap-10 text-xl">
+      <ScrollToTop />
       <ListingsOverview />
       <ListingsToFulfill />
       <DisputedListings />
