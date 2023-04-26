27edb50432acf5cbaee17a9d1c32375691717328
Kennette Maddela <kennettemaddela@kennettes-mbp.lan>
2023-04-26 10:24:40 -0400

    Implement basic struct of scroll to top button

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,19 @@
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
-import { BrowserRouter as Router } from "react-router-dom"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import GlobalContextProviderLayout from "./components/layout/GlobalContextProviderLayout"
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import GlobalContextProviderLayout from "./components/layout/GlobalContextProviderLayout";
 
 // Query Library
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Router>
-    <QueryClientProvider client={queryClient}>
-      <GlobalContextProviderLayout>
-        <App />
-      </GlobalContextProviderLayout>
-    </QueryClientProvider>
-  </Router>
-)
+	<Router>
+		<QueryClientProvider client={queryClient}>
+			<GlobalContextProviderLayout>
+				<App />
+			</GlobalContextProviderLayout>
+		</QueryClientProvider>
+	</Router>
+);
