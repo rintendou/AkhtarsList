3bcbbacf3e33c5ee77e41402f17ee288f2794d75
Kennette Maddela <kennettemaddela@kennettes-mbp.lan>
2023-04-26 09:12:09 -0400

    Increase spacing of login and register routes

--- client/src/components/routes/login/Login.tsx
+++ client/src/components/routes/login/Login.tsx
@@ -1,31 +1,31 @@
-import useAuthContext from "../../../lib/hooks/context-hooks/useAuthContext"
-import { useLocation, Navigate } from "react-router-dom"
+import useAuthContext from "../../../lib/hooks/context-hooks/useAuthContext";
+import { useLocation, Navigate } from "react-router-dom";
 
-import LoginForm from "./LoginForm"
-import ScrollToTop from "../../../lib/util/components/ScrollToTop"
+import LoginForm from "./LoginForm";
+import ScrollToTop from "../../../lib/util/components/ScrollToTop";
 
 const Login = () => {
-  const { auth } = useAuthContext()
+	const { auth } = useAuthContext();
 
-  const location = useLocation()
-  const didRegisterSuccessfully = location.state?.didRegisterSuccessfully
-  const successMessage = location.state?.successMessage
-  const errorMessage = location.state?.errorMessage
+	const location = useLocation();
+	const didRegisterSuccessfully = location.state?.didRegisterSuccessfully;
+	const successMessage = location.state?.successMessage;
+	const errorMessage = location.state?.errorMessage;
 
-  return (
-    <div className="min-h-screen flex flex-col justify-center">
-      <ScrollToTop />
-      {!auth._id ? (
-        <LoginForm
-          didRegisterSuccessfully={didRegisterSuccessfully}
-          successMessage={successMessage}
-          errorMessageFromOtherRoute={errorMessage}
-        />
-      ) : (
-        <Navigate to="/" />
-      )}
-    </div>
-  )
-}
+	return (
+		<div className="min-h-screen flex flex-col justify-center p-20">
+			<ScrollToTop />
+			{!auth._id ? (
+				<LoginForm
+					didRegisterSuccessfully={didRegisterSuccessfully}
+					successMessage={successMessage}
+					errorMessageFromOtherRoute={errorMessage}
+				/>
+			) : (
+				<Navigate to="/" />
+			)}
+		</div>
+	);
+};
 
-export default Login
+export default Login;
