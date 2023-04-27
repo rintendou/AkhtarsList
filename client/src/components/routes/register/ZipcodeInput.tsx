4d4f75436e7a64cd0e3e13e9c8d4d617b14ef92e
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-26 20:47:42 -0400

    Limit zipcode to 5 chars

---
+++ client/src/components/routes/register/ZipcodeInput.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+type Props = {
+  name: string
+  type: "text" | "email" | "password" | "number"
+  placeholder: string
+  twClasses?: string
+  disabled?: boolean
+}
+
+const ZipcodeInput = React.forwardRef<HTMLInputElement, Props>(
+  ({ name, type = "text", placeholder, twClasses, ...rest }, ref) => {
+    return (
+      <div className="relative z-0">
+        <input
+          {...rest}
+          id={name}
+          type={type}
+          placeholder=""
+          maxLength={5}
+          className={`${twClasses} pt-5 pl-5 p-2 block w-full px-0 mt-0 bg-transparent border-2 appearance-none focus:outline-none focus:ring-0 focus:border-secondary border-gray-200 rounded-md dark:bg-black dark:text-primary dark:focus:border-tertiary`}
+          ref={ref}
+        />
+        <label
+          htmlFor={name}
+          className="absolute duration-200 ease-in-out top-3 left-3 -z-1 origin-0 text-secondary dark:text-primary"
+        >
+          {placeholder}
+        </label>
+      </div>
+    )
+  }
+)
+
+export default ZipcodeInput
