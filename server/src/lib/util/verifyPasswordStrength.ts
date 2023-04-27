19971fd97666b77c7ce836b8f3bc9617c352c339
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-27 14:17:08 -0400

    Clean up password strength util fn validator

--- server/src/lib/util/verifyPasswordStrength.ts
+++ server/src/lib/util/verifyPasswordStrength.ts
@@ -4,27 +4,17 @@ const verifyPasswordStrength = (password: string): boolean => {
   const regexNumeric = /[0-9]/
   const regexSpecialChar = /[!@#$%^&*]/
 
-  if (password.length < 8) {
+  if (
+    password.length < 8 ||
+    !regexUpperCase.test(password) ||
+    !regexLowerCase.test(password) ||
+    !regexNumeric.test(password) ||
+    !regexSpecialChar.test(password)
+  ) {
     return false
+  } else {
+    return true
   }
-
-  if (!regexUpperCase.test(password)) {
-    return false
-  }
-
-  if (!regexLowerCase.test(password)) {
-    return false
-  }
-
-  if (!regexNumeric.test(password)) {
-    return false
-  }
-
-  if (!regexSpecialChar.test(password)) {
-    return false
-  }
-
-  return true
 }
 
 export default verifyPasswordStrength
