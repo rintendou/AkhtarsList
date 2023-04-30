98475d45152c68b4771e81635f2220aff3438603
Kennette James Basco <kenji.maddela01@gmail.com>
2023-04-29 21:01:23 -0400

    Organize imports of layout

--- client/src/components/layout/body/Body.tsx
+++ client/src/components/layout/body/Body.tsx
@@ -1,17 +1,14 @@
-import React from "react";
-import FixedScrollToTop from "../../ui/FixedScrollToTop";
-
 type Props = {
-	children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 const Body = ({ children }: Props) => {
-	return (
-		<main className="min-h-screen flex flex-col space-y-4 h-full dark:bg-secondary">
-			<div className="h-20 bg-secondary"></div>
-			{children}
-		</main>
-	);
-};
+  return (
+    <main className="min-h-screen flex flex-col space-y-4 h-full dark:bg-secondary">
+      <div className="h-20 bg-secondary"></div>
+      {children}
+    </main>
+  )
+}
 
-export default Body;
+export default Body
