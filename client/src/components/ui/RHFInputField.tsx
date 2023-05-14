e6cbbeb075e672b500bcdac5a566560df662bc42
Kennette James Basco <kenji.maddela01@gmail.com>
2023-05-14 19:53:21 -0400

    Implement RHF input field

---
+++ client/src/components/ui/RHFInputField.tsx
@@ -0,0 +1,31 @@
+import { UseFormRegisterReturn } from "react-hook-form"
+
+type Props = {
+  id: string
+  type?: string
+  register: UseFormRegisterReturn
+  error?: string
+}
+
+const RHFInputField = ({ id, type = "text", register, error }: Props) => {
+  return (
+    <div className="relative z-0 mb-8">
+      <input
+        id={id}
+        type={type}
+        placeholder=""
+        className="pt-5 p-2 block w-full mt-0 bg-transparent border-2 appearance-none focus:outline-none focus:ring-0 focus:border-secondary border-gray-200 pl-5 rounded-md"
+        {...register}
+      />
+      <label
+        htmlFor={id}
+        className="absolute duration-200 ease-in-out top-3 left-3 -z-1 origin-0 text-secondary"
+      >
+        {id}
+      </label>
+      <p>{error}</p>
+    </div>
+  )
+}
+
+export default RHFInputField
