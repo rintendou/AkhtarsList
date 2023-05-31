53991499a80d75d4b8ebd95149aff4de661e3550
Kennette James Basco <kenji.maddela01@gmail.com>
2023-05-31 18:39:07 -0400

    Refactor loginFormSchema and validate pw strength

--- common/validations/loginFormValidator.ts
+++ common/validations/loginFormValidator.ts
@@ -1,11 +1,18 @@
 import { z } from "zod"
 
+const strongPasswordRegex =
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
 export const loginFormSchema = z.object({
   username: z
-    .string({ required_error: "Username is required!" })
-    .toLowerCase()
-    .min(3, "Username must contain at least 3 characters"),
-  password: z.string({ required_error: "Password is required!" }),
+    .string()
+    .min(3, { message: "Username must contain at least 3 characters" })
+    .trim()
+    .toLowerCase(),
+  password: z.string().min(8).regex(strongPasswordRegex, {
+    message:
+      "Passwords must contain at least one uppercase letter, one lowercase letter, one digit, one special character, and be a minimum of 8 characters long.",
+  }),
 })
 
 export type loginFormType = z.infer<typeof loginFormSchema>
